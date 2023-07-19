b4d7735d7d2b5896d50da3e6b523521a1f4e2c1f
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2023-07-19 14:54:21 +0300

    feat: add tooltip

--- src/features/navigation/navigation.jsx
+++ src/features/navigation/navigation.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Tooltip } from "react-tooltip";
 
 import { Link } from "react-scroll";
 
 import { ReactComponent as HomeIcon } from "./images/home.svg";
 import { ReactComponent as ContactIcon } from "./images/contact.svg";
-import { ReactComponent as CVIcon } from "./images/cv.svg";
+import { ReactComponent as EducationIcon } from "./images/cv.svg";
 import { ReactComponent as PortfolioIcon } from "./images/portfolio.svg";
+import { ReactComponent as WorkIcon } from "./images/work.svg";
 
 import "./navigation.css";
 
@@ -18,17 +20,55 @@ export const Navigation = () => {
         to="home"
         smooth={true}
         duration={500}
+        data-tooltip-id="tooltip-home"
+        data-tooltip-content="Home"
       >
         <HomeIcon className="nav__icon" />
+        <Tooltip className="tooltip" id="tooltip-home" />
       </Link>
-      <Link className="nav__item" to="work" smooth={true} duration={500}>
-        <CVIcon className="nav__icon" />
+      <Link
+        className="nav__item"
+        to="work"
+        smooth={true}
+        duration={500}
+        data-tooltip-id="tooltip-work"
+        data-tooltip-content="Work"
+      >
+        <WorkIcon className="nav__icon" />
+        <Tooltip className="tooltip" id="tooltip-work" />
       </Link>
-      <Link className="nav__item" to="portfolio" smooth={true} duration={500}>
+      <Link
+        className="nav__item"
+        to="education"
+        smooth={true}
+        duration={500}
+        data-tooltip-id="tooltip-education"
+        data-tooltip-content="Education"
+      >
+        <EducationIcon className="nav__icon" />
+        <Tooltip className="tooltip" id="tooltip-education" />
+      </Link>
+      <Link
+        className="nav__item"
+        to="portfolio"
+        smooth={true}
+        duration={500}
+        data-tooltip-id="tooltip-portfolio"
+        data-tooltip-content="Portfolio"
+      >
         <PortfolioIcon className="nav__icon" />
+        <Tooltip className="tooltip" id="tooltip-portfolio" />
       </Link>
-      <Link className="nav__item" to="contacts" smooth={true} duration={500}>
+      <Link
+        className="nav__item"
+        to="contacts"
+        smooth={true}
+        duration={500}
+        data-tooltip-id="tooltip-contacts"
+        data-tooltip-content="Contacts"
+      >
         <ContactIcon className="nav__icon" />
+        <Tooltip className="tooltip" id="tooltip-contacts" />
       </Link>
     </nav>
   );
